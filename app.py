121d6f1186a50c4554e225ad133a939c3668db96
Dimitri Kindelberger <dimitri.kindelberger@telecom-sudparis.eu>
2026-09-08 21:31:37 +0200

    description page

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def health_check():
         "service": "flask-app"
     }), 200
 
+@app.route('/description', methods=['GET'])
+def message():
+    return "<p>The purpose of this website is to help me practice developing and optimizing an entire project using a DevSecOps approach.</p> <h1>Specifically: Develop a web microservice, containerize it properly, automate testing, build an automated CI/CD pipeline</h1>"
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
